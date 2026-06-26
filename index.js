9ac743d813effb7c87718f4cb7000ab928e91bbd
unknown <bkc.vns@gmail.com>
2026-06-26 12:25:19 +0530

    Data Update work done

--- index.js
+++ index.js
@@ -9,17 +9,66 @@ let students = [
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API IS RUNNING");
 });
 
-app.get("/students",(req,res)=>{
+// Data Retrieve
+app.get("/students", (req, res) => {
     res.json({
-        message:"All Students",
+        message: "All Students",
         data: students
     });
 });
+app.get("/students/:id", (req, res) => {
+    const id = req.params.id;
+    const student = students.find(s => s.id == id);
+    if(!student)
+    {
+        return res.status(404).json({
+            message:"Student Not Found"
+        });
+    }
 
-app.listen(3000,()=>{
+    res.json({
+        message: "Student Found",
+        data: student
+    });
+});
+// Data Store
+app.post("/students", (req, res) => {
+    const { id, name, city } = req.body;
+    const newStudent = { id, name, city };
+    students.push(newStudent);
+    res.json({
+        message: "Record Added",
+        student: newStudent,
+        data: students
+    })
+});
+
+// Data Update
+app.put("/students/:id", (req, res) => {
+    // const id = req.params.id;
+    const { id } = req.params;
+    const student = students.find(s => s.id == id);
+    // if student found = value....
+    if(!student)
+    {
+        return res.status(404).json({
+            message:"Student Not Found"
+        });
+    }
+    student.name = req.body.name;
+    student.city = req.body.city;
+
+    res.json({
+        message:"Record updated",
+        student
+    });
+
+});
+
+app.listen(3000, () => {
     console.log("Server Started");
 });
